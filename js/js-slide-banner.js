c0329b9b81b6ef947198c5d2cd81c96f68276010
Web-Yumelead <65036551+Web-Yumelead@users.noreply.github.com>
2020-05-11 23:58:51 +0900

    add slide-base image

--- js/js-slide-banner.js
+++ js/js-slide-banner.js
@@ -1,37 +1,31 @@
-$(function() {
-  // 置換の対象とするclass属性。
-  var $elem = $('.js-slide-banner');
-  // 置換の対象とするsrc属性の末尾の文字列。
-  var sp = '_sp.';
-  var pc = '_pc.';
-  // 画像を切り替えるウィンドウサイズ。
-  var replaceWidth = 800;
+$(window).on('load resize', function(){
 
-  function imageSwitch() {
-    // ウィンドウサイズを取得する。
-    var windowWidth = parseInt(window.innerWidth);
+  var w = $(window).width();
+  var x = 768; //画像を差し替えを実行するウィンドウサイズ
 
-    // ページ内にあるすべての`.js-image-switch`に適応される。
-    $elem.each(function() {
-      var $this = $(this);
-      // ウィンドウサイズが768px以上であれば_spを_pcに置換する。
-      if(windowWidth >= replaceWidth) {
-        $this.attr('src', $this.attr('src').replace(sp, pc));
+  if (w <= x) {
 
-      // ウィンドウサイズが768px未満であれば_pcを_spに置換する。
-      } else {
-        $this.attr('src', $this.attr('src').replace(pc, sp));
+    var before = '_pc.',
+        after = '_sp.';
+
+    replaceImg();
+
+  } else {
+
+    var before = '_sp.',
+        after = '_pc.';
+
+    replaceImg();
+
+  }
+
+  function replaceImg(){
+    $('img[src*=pc_],img[src*=sp_]').each(function(){
+      var img = $(this).attr('src').replace(before, after);
+      if( $(this).attr('src').match(before) ) {
+        $(this).attr('src', img);
       }
     });
   }
-  imageSwitch();
 
-  // 動的なリサイズは操作後0.2秒経ってから処理を実行する。
-  var resizeTimer;
-  $(window).on('resize', function() {
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(function() {
-      imageSwitch();
-    }, 200);
-  });
-});
+})
